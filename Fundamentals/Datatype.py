fd38eed802c0b849396629d3a3f77bb9a67ab91d
Rezwanul Rimel <rezwanul.rimel97@gmail.com>
2026-05-31 16:01:11 +0600

    added Datatypes

--- Fundamentals/Datatype.py
+++ Fundamentals/Datatype.py
@@ -2,3 +2,60 @@ Rime=("jnr sqa engineer")
 print(Rime)
 
 # all data types example
+
+# String (str)
+name = "Rimel"
+print("String:", name)
+
+# Integer (int)
+age = 29
+print("Integer:", age)
+
+# Float (float)
+height = 5.8
+print("Float:", height)
+
+# Boolean (bool)
+is_sqa = True
+print("Boolean:", is_sqa)
+
+# List
+fruits = ["Apple", "Banana", "Mango"]
+print("List:", fruits)
+
+# Tuple
+colors = ("Red", "Green", "Blue")
+print("Tuple:", colors)
+
+# Set
+numbers = {1, 2, 3, 4, 5}
+print("Set:", numbers)
+
+# Dictionary
+person = {
+    "name": "Rimel",
+    "age": 29,
+    "profession": "SQA Engineer"
+}
+print("Dictionary:", person)
+
+# NoneType
+data = None
+print("NoneType:", data)
+
+# Complex
+complex_num = 2 + 3j
+print("Complex:", complex_num)
+
+# Type Checking
+print("\nType Checking:")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_sqa))
+print(type(fruits))
+print(type(colors))
+print(type(numbers))
+print(type(person))
+print(type(data))
+print(type(complex_num))
